ec316d3ed27ae8234d6f7d8b81c8e205ebfcb0b0
Anonymous Hacker <suprodipbhattacharjee1@gmail.com>
2025-09-20 19:30:39 +0530

    made the create question api

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -1,4 +1,32 @@
-export const createQuestion = (req, res) => {};
+import Question from "../models/questionModel.js";
+
+export const createQuestion = async (req, res) => {
+  try {
+    const { title, options, ansIndex } = req.body;
+
+    if (!title || !options || !ansIndex) {
+      return res.status(400).json({
+        msg: "missing fields",
+      });
+    }
+
+    const question = await Question.create({
+      title,
+      options,
+      ansIndex,
+    });
+
+    res.status(201).json({
+      msg: "question created successfully",
+      question,
+    });
+  } catch (error) {
+    console.error("Error creating question:", error);
+    res.status(500).json({
+      msg: "server error",
+    });
+  }
+};
 
 export const getQuestion = (req, res) => {};
 
